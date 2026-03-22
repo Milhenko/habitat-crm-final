5a2e09a1eb797d7f057fa3053bbb47d15c2d7a99
MilenkoSuratiConstructor <ventas@milenkosurati.com>
2026-03-22 16:25:39 -0500

    arreglo colores

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@ import PropertyForm from "@/components/PropertyForm";
 
 export default function Home() {
   return (
-    <main className="min-h-screen py-12 px-4 sm:px-6 lg:px-8 bg-gray-50 dark:bg-zinc-950">
+    /* Eliminamos bg-gray-50 y bg-zinc-950 para que brille el degradado que pusimos en globals.css */
+    <main className="min-h-screen py-12 px-4 sm:px-6 lg:px-8 text-rich-black">
       <div className="max-w-[1600px] mx-auto">
         <Header />
         <PropertyForm />
